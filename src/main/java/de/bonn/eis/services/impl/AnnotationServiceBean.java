c87266ef39303c6aa9340ccc40b4bf2bc0c27094
aw3s0me <akorovin00@gmail.com>
2017-03-18 17:13:16 +0100

    adding dependencies anno4j, jena

--- src/main/java/de/bonn/eis/services/impl/AnnotationServiceBean.java
+++ src/main/java/de/bonn/eis/services/impl/AnnotationServiceBean.java
@@ -1,14 +1,26 @@
 package de.bonn.eis.services.impl;
 
+import com.github.anno4j.Anno4j;
 import de.bonn.eis.models.Annotation;
 import de.bonn.eis.services.AnnotationService;
-
-import java.util.UUID;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.tdb.TDBFactory;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.config.RepositoryConfigException;
 
 /**
  * Created by korovin on 3/18/2017.
  */
 public class AnnotationServiceBean implements AnnotationService {
+    private String schemaName = "schema";
+    private Dataset dataset;
+    private Anno4j anno4j;
+
+    public AnnotationServiceBean() throws RepositoryConfigException, RepositoryException {
+        this.dataset = TDBFactory.createDataset(schemaName);
+        this.anno4j = new Anno4j();
+    }
+
     @Override
     public Annotation create(Annotation annotation) {
         return null;
@@ -20,7 +32,7 @@ public class AnnotationServiceBean implements AnnotationService {
     }
 
     @Override
-    public void delete(UUID id) {
-
+    public boolean delete(String id) {
+        return false;
     }
 }
